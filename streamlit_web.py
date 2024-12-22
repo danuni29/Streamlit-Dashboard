eef1ed5a3ac2679f34bd6e19a0f33359cc18ee38
Da Eun Ha <117696370+danuni29@users.noreply.github.com>
2024-12-23 05:54:45 +0900

    Update streamlit_web.py

--- streamlit_web.py
+++ streamlit_web.py
@@ -165,7 +165,7 @@ def main():
 
     elif menu == "챗봇":
         st.title("Agricultural Chat Assistant")
-        OPENAI_API_KEY = "OdvbMA5NLHI_qlTR0A"
+        OPENAI_API_KEY = st.secrets["openai"]["api_key"]
         chat_model = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-4o", temperature=0.8)
 
         # 시스템 프롬프트 설정
